8db393e7560989b7a1eb4c1158b69a02274e210a
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2024-02-28 17:16:48 +0530

    Setup the utils

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+// Enable CORS with the specified origin and credentials
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// Parse incoming requests with JSON payloads
+app.use(express.json({ limit: "16kb" }));
+
+// Parse incoming requests with URL-encoded payloads
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Serve static files from the "public" directory
+app.use(express.static("public"));
+
+
+app.use(cookieParser())
+
+export { app };
